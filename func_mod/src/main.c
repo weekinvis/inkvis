00fc6f4e7c446be1fe2b86d4b2193fc910a45a12
inkvs <obernardo99@gmail.com>
2026-04-16 14:48:23 -0300

    Remotando o codigo

---
+++ func_mod/src/main.c
@@ -0,0 +1,11 @@
+/* Esse arquivo eh temporario e so serve para testar o modulo de funcoes. */
+#include <stdio.h>
+
+#define SUCESSO 0
+
+int 
+main(const int argc, const char * argv[])
+{
+
+    return SUCESSO;
+}
